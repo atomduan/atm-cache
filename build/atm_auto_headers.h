76c0938a2d62d2f31104c92aced47133e4c8c7bf
juntaoduan <juntaoduan@localhost.dell>
2017-04-13 19:44:05 +0800

    fix a format in Makefile

---
+++ build/atm_auto_headers.h
@@ -0,0 +1,51 @@
+
+
+#ifndef ATM_HAVE_UNISTD_H
+#define ATM_HAVE_UNISTD_H  1
+#endif
+
+
+#ifndef ATM_HAVE_INTTYPES_H
+#define ATM_HAVE_INTTYPES_H  1
+#endif
+
+
+#ifndef ATM_HAVE_LIMITS_H
+#define ATM_HAVE_LIMITS_H  1
+#endif
+
+
+#ifndef ATM_HAVE_SYS_PARAM_H
+#define ATM_HAVE_SYS_PARAM_H  1
+#endif
+
+
+#ifndef ATM_HAVE_SYS_MOUNT_H
+#define ATM_HAVE_SYS_MOUNT_H  1
+#endif
+
+
+#ifndef ATM_HAVE_SYS_STATVFS_H
+#define ATM_HAVE_SYS_STATVFS_H  1
+#endif
+
+
+#ifndef ATM_HAVE_CRYPT_H
+#define ATM_HAVE_CRYPT_H  1
+#endif
+
+
+#ifndef ATM_LINUX
+#define ATM_LINUX  1
+#endif
+
+
+#ifndef ATM_HAVE_SYS_PRCTL_H
+#define ATM_HAVE_SYS_PRCTL_H  1
+#endif
+
+
+#ifndef ATM_HAVE_SYS_VFS_H
+#define ATM_HAVE_SYS_VFS_H  1
+#endif
+

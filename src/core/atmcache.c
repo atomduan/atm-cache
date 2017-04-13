929ca198d843809265f3b72d6c1cbf7da2b39423
atomduan <atomduan@gmail.com>
2017-04-13 22:30:51 +0800

    add config path

--- src/core/atmcache.c
+++ src/core/atmcache.c
@@ -18,14 +18,16 @@
 #include <atm_core.h>
 
 static void atm_initialize() {
+    printf("atm_initialize enter......\n");
     atm_hash_init_siphash_seed();
 }
 
 static void atm_service() {
-
+    printf("atm_service enter......\n");
 }
 
 int main(int argc, char **argv) {
+    printf("Hello world ......\n");
     atm_initialize();
     atm_service();
     return 0;

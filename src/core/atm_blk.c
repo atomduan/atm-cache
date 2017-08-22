9ca5d23eeffb27158410365b047be2be1aa5ddd2
atomduan <atomduan@gmail.com>
2017-08-22 21:04:45 +0800

    add pool struct

--- src/core/atm_blk.c
+++ src/core/atm_blk.c
@@ -2,6 +2,15 @@
 /*
  * Private
  * */
+static atm_bool_t
+atm_blk_pool_release(atm_blk_t *block);
+static atm_blk_t *
+atm_blk_pool_fetch();
+static atm_blk_t *
+atm_blk_new_raw();
+static void
+atm_blk_free_raw();
+
 static atm_T_t ATM_BLK_TYPE = {
     NULL,/* sepc */
     NULL,/* match */
@@ -18,15 +27,36 @@ static atm_T_t ATM_BLK_TYPE = {
 atm_T_t *ATM_BLK_T = &ATM_BLK_TYPE;
 
 
-//static atm_list_t *blk_pool;
+static atm_list_t *blk_pool = NULL;
 
 
 /* ---------------------IMPLEMENTATIONS--------------------------- */
 /*
- * Public
+ * Private
  * */
-atm_blk_t *
-atm_blk_new()
+static atm_bool_t
+atm_blk_pool_release(atm_blk_t *block)
+{
+    atm_bool_t res = ATM_FALSE;
+    if (blk_pool == NULL) 
+        res = ATM_FALSE;
+    return res;
+}
+
+
+static atm_blk_t *
+atm_blk_pool_fetch()
+{
+    atm_blk_t *res = NULL;
+    if (blk_pool != NULL) {
+        //TODO need implements
+    }
+    return res;
+}
+
+
+static atm_blk_t *
+atm_blk_new_raw()
 {
     atm_blk_t *res = NULL;
     int size = ATM_BLK_DFT_LEN;
@@ -40,12 +70,39 @@ atm_blk_new()
 }
 
 
-void
-atm_blk_free(void *block)
+static void
+atm_blk_free_raw(void *block)
 {
     atm_blk_t *b = block;
     if (b != NULL) {
         atm_free(b->head);
         atm_free(b);
+    }
+}
+
+
+/*
+ * Public
+ * */
+atm_blk_t *
+atm_blk_new()
+{
+    atm_blk_t *res = NULL;
+    res=atm_blk_pool_fetch();
+    if (res == NULL) {
+        res = atm_blk_new_raw();
+    }
+    return res;
+}
+
+
+void
+atm_blk_free(void *block)
+{
+    if (block != NULL) {
+        atm_blk_t *b = block;
+        if (!atm_blk_pool_release(b)) {
+            atm_blk_free_raw(block);
+        }
     }
 }

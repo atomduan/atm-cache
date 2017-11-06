9b6458a8b61b875ea147aeb9a88110985386760b
atomduan <atomduan@gmail.com>
2017-11-06 20:57:05 +0800

    add new test

---
+++ src/test/misc/print.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct TS {
+    struct TS *pre;
+    struct TS *next;
+    int data;
+};
+
+void initList(struct TS * ts, int a, int b)
+{
+    ts->pre = ts;
+    ts->next = ts;
+    ts->data = 1024 + a + b;
+}
+
+int main()
+{
+    struct TS * list = malloc(sizeof(struct TS) * 10);
+    int i=0;
+    for (i = 0; i < 10; ++i) {
+        initList(&list[i], 1, i);
+        printf("current:0x%p, pre:0x%p, next:0x%p\n", &list[i], list[i].pre, list[i].next);
+        printf("current:0x%p, pre:0x%p, next:0x%p, data=%d\n", &list[i], list[i].pre, list[i].next, list[i].data);
+    }
+    return 0;
+}

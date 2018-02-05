1693510add693a5691777f2b0da02350f3e3b6d3
atomduan <atomduan@gmail.com>
2018-02-05 18:23:47 +0800

    mod asm

--- src/misc/asmsnippy.c
+++ src/misc/asmsnippy.c
@@ -7,16 +7,8 @@ int main(int argc, char **argv)
     int b=5;
     int c;
     c = a+b;
-    __asm(
-	"movl	%edi, -20(%rbp)\n"
-	"movq	%rsi, -32(%rbp)\n"
-	"movl	$1, -4(%rbp)\n"
-	"movl	$5, -8(%rbp)\n"
-	"movl	-4(%rbp), %edx\n"
-	"movl	-8(%rbp), %eax\n"
-	"addl	%edx, %eax\n"
-	"movl	%eax, -12(%rbp)\n"
-	"movl	-12(%rbp), %eax\n"
+    __asm__ (
+	"    mov    %ebx, %esi;  "
             );
     return c;
 }

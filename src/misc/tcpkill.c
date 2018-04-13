693d8946525a645faa3b4e1df95ddafb6f2f5686
atomduan <atomduan@gmail.com>
2018-04-13 18:18:16 +0800

    add art code

--- src/misc/tcpkill.c
+++ src/misc/tcpkill.c
@@ -8,3 +8,32 @@ int main(int argc, char **argv)
     printf("not implemented yet......pd point%p", pd);
     return 0;
 }
+
+/* from tcpdump util.c. */
+char *
+copy_argv(char **argv)
+{
+    char **p, *buf, *src, *dst;
+    u_int len = 0;
+    
+    p = argv;
+    if (*p == 0)
+        return (0);
+    
+    while (*p)
+        len += strlen(*p++) + 1;
+    
+    if ((buf = (char *)malloc(len)) == NULL)
+        err(1, "copy_argv: malloc");
+    
+    p = argv;
+    dst = buf;
+    
+    while ((src = *p++) != NULL) {
+        while ((*dst++ = *src++) != '\0')
+            ;
+        dst[-1] = ' ';
+    }
+    dst[-1] = '\0';
+    return (buf);
+}
